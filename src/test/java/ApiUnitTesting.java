6dfb5a7964018010b00e62b0ff85450a8e3c365d
Christian S <chris.salapang@gmail.com>
2021-05-17 01:45:34 +0800

    added post test

--- src/test/java/ApiUnitTesting.java
+++ src/test/java/ApiUnitTesting.java
@@ -1,12 +1,26 @@
-import static org.junit.Assert.*;
-
-import org.junit.Test;
+import org.json.simple.JSONObject;
+import static io.restassured.RestAssured.*;
+import org.testng.annotations.Test;
 
 public class ApiUnitTesting {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    public void testingpost() {
+        JSONObject post = new JSONObject();
+
+        post.put("name", "Kevin");
+        post.put("job", "Barista");
+
+        System.out.println(post.toJSONString());
+
+        given().
+                body(post.toJSONString()).
+        when().
+                post ("https://reqres.in/api/users").
+        then().
+            statusCode(201);
+    }
+	
+	
 
 }
